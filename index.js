f650631318e23492bf087d2a463a705db00b9072
Gabin <hello@gabinaureche.com>
2017-11-07 22:36:02 +0100

    Improve design

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
 const getPort = require('get-port');
 const opn = require('opn');
+const pug = require('pug');
 
-fs.readdir(process.cwd(), (err, files) => {
-    if (err) {
-        // eslint-disable-next-line no-console
-        console.error(err);
-        process.exit(1);
-    }
+const template = fs.readFileSync(path.join(__dirname, 'index.pug'), 'utf8');
+const render = pug.compile(template);
 
-    const svgs = files
-        .filter(file => /\.svg$/i.test(file))
-        .map(file => fs.readFileSync(path.join(process.cwd(), file), 'utf8'));
+const dir = process.cwd();
+const files = fs.readdirSync(dir);
+const svgs = files.filter(file => /\.svg$/i.test(file)).map(filename => ({
+    filename,
+    path: path.join(dir, filename),
+    code: fs.readFileSync(path.join(dir, filename), 'utf8')
+}));
 
-    const server = http.createServer((req, res) => {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(
-            `<!doctype html><html><head></head><body>${svgs.join(
-                ''
-            )}</body></html>`
-        );
-        process.exit();
-    });
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(render({ dir, svgs }));
+});
 
-    getPort().then(port => {
-        server.listen(port);
-        opn(`http://127.0.0.1:${port}`);
-    });
+getPort().then(port => {
+    server.listen(port);
+
+    const URL = `http://127.0.0.1:${port}`;
+    opn(URL);
+
+    console.log('Serving svg files from:');
+    console.log(dir);
+    console.log(URL);
 });
