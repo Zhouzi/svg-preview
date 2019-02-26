f08a8174c8105b5b8c0c19baa0342f7e8b991c81
Gabin <hello@gabinaureche.com>
2019-02-26 11:37:11 +0100

    Prevent conflicts between svgs

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
-const path = require('path');
 const getPort = require('get-port');
 const serveSvgs = require('./serveSvgs');
 
 getPort().then(port => {
-    const input = process.argv[2] || '.';
-    const dir = path.join(process.cwd(), input);
-    serveSvgs(dir, port);
+    serveSvgs(process.argv[2] || '.', port);
 });
--- readSvgs.js
+++ readSvgs.js
@@ -1,13 +1,47 @@
 const path = require('path');
 const fs = require('fs');
+const cheerio = require('cheerio');
 
 function readSvgs(dir) {
     const files = fs.readdirSync(dir);
-    return files.filter(file => /\.svg$/i.test(file)).map(filename => ({
-        filename,
-        path: path.join(dir, filename),
-        code: fs.readFileSync(path.join(dir, filename), 'utf8')
-    }));
+    return files
+        .filter(file => path.parse(file).ext === '.svg')
+        .map(filename => {
+            const content = fs.readFileSync(path.join(dir, filename), 'utf8');
+
+            // svgs are going to be all listed on the same page so we need to make
+            // sure they are not conflicting by resolving the <use>s
+            const $ = cheerio.load(content, {
+                xmlMode: true
+            });
+            const $svg = $('svg');
+
+            $svg.find('use').each((index, use) => {
+                const $use = $(use);
+                const id = $use.attr('xlink:href');
+                const $path = $svg.find(id).clone();
+
+                // Copy the attributes to the path
+                const attrs = $use.get(0).attribs;
+                Object.keys(attrs).forEach(attr => {
+                    $path.attr(attr, attrs[attr]);
+                });
+
+                // Replace the <use> with the path it points to
+                $use.replaceWith($path);
+            });
+
+            // All defs must have been resolved by now
+            // so we can safely remove them along with [id]s
+            $svg.find('defs').remove();
+            $svg.find('[id]').each((index, child) => $(child).removeAttr('id'));
+
+            return {
+                filename,
+                path: path.join(dir, filename),
+                code: $.html($svg)
+            };
+        });
 }
 
 module.exports = readSvgs;
